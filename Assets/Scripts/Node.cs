8b680d31855c3380d4572bf4c1c1714d7a6c2406
Krezme <karakonjo5818@gmail.com>
2022-10-17 10:42:20 +0100

    Created a grid and Drawing it with gizmos

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AStar{
+    public class Node
+    {
+        public bool walkable;
+
+        public Vector3 worldPosition;
+
+        public Node(bool _walkable, Vector3 _worldPos)
+        {
+            walkable = _walkable;
+            worldPosition = _worldPos;
+        }
+    }
+}
